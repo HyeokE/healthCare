d363dcc4b89a67b5a9b7808fc95faee71d74e02a
HYEOK_E <jhjeong00@gmail.com>
2022-03-31 02:32:45 +0900

    chore: layout modify

--- src/styles/layouts.ts
+++ src/styles/layouts.ts
@@ -1,18 +1,4 @@
-import { motion } from 'framer-motion';
 import styled from 'styled-components';
-
-export const TopMargin = styled.div`
-  height: 50px;
-  @media (max-width: 500px) {
-    height: 30px;
-  }
-`;
-export const SmallTopMargin = styled.div`
-  height: 30px;
-`;
-
-export const Tage = styled.div``;
-
 export const LayoutContainer = styled.div`
   position: relative;
   width: 100%;
@@ -27,22 +13,4 @@ export const ContainerInner = styled.div`
   height: 100%;
   max-width: calc(100% - 48px);
   margin: 0 auto;
-`;
-export const CardList = styled(motion.section)`
-  display: flex;
-  flex-wrap: wrap;
-`;
-
-export const BannerImage = styled.img`
-  height: 500px;
-`;
-export const BannerWrapper = styled.div`
-  height: 200px;
-  width: 100%;
-  overflow: hidden;
-  display: flex;
-  justify-content: center;
-  @media (max-width: 500px) {
-    height: 180px;
-  }
 `;
